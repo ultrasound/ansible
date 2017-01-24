53286f56f74229c079788d8c872a16a2fd9cd66e
Nick <ultrasound2k@gmail.com>
2017-01-25 01:31:00 +0900

    edited app view.py added models.py in app

--- webapps/blogs/ultrasound/api/views.py
+++ webapps/blogs/ultrasound/api/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .models import Post
 
 # Create your views here.
 
 def post_list(request):
-    data = [
-        {'id':1, 'title': 'title 1'},
-    ]
-    return JsonResponse(data, safe=False)
+    post_list = []
+    for post in Post.objects.all():
+        post_list.append({
+            'id':post.id,
+            'title':post.title,
+            'content':post.content,
+        })
+    return JsonResponse(post_list, safe=False)
