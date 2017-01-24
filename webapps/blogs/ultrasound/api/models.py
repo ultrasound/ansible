a48a69ddecdfb7785cf575f69399dac822b2ec9d
Nick <ultrasound2k@gmail.com>
2017-01-25 03:21:56 +0900

    fixed typo

--- webapps/blogs/ultrasound/api/models.py
+++ webapps/blogs/ultrasound/api/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Post(models.Model):
     user_id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=10)
-    is_author = models.CharField(max_lenth=10)
+    is_author = models.CharField(max_length=10)
     article_title = models.CharField(max_length=100)
     article_content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
