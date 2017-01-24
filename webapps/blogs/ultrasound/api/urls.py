089fff5e68e099cb1eb2af7f761dc0ef09279a7a
Nick <ultrasound2k@gmail.com>
2017-01-25 03:34:31 +0900

    delete login page

--- webapps/blogs/ultrasound/api/urls.py
+++ webapps/blogs/ultrasound/api/urls.py
@@ -3,13 +3,5 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    #url(r'^$', views.post_list),
-    url(r'^signup/$', 'api.views.signup', name='signup'),
-    url(r'^signup_ok/$', TemplateView.as_view(
-        template_name='registration/signup_ok.html'
-    ), name='signup_ok'),
-    url(r'^$', 'django.contrib.auth.views.login', name='login_url'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', {
-        'next_page': '/login/',
-    }, name='logout_url'),
+    url(r'^$', views.post_list),
 ]
--- webapps/blogs/ultrasound/api/views.py
+++ webapps/blogs/ultrasound/api/views.py
@@ -5,25 +5,3 @@ from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
-from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
-from django.contrib.auth.forms import UserCreationForm
-
-def signup(request):
-    """signup
-    to register users
-    """
-    if request.method == "POST":
-        userform = UserCreationForm(request.POST)
-        if userform.is_valid():
-            userform.save()
-
-        return HttpResponseRedirect(
-            reverse("signup_ok")
-        )
-    elif request.method == "GET":
-        userform = UserCreationForm()
-
-    return render(request, "registration/signup.html", {
-        "userform": userform,
-    })
