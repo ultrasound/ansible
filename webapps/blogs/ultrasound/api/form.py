5aef27cfa57c12a58da8bd6133aeaf302d90916e
Nick <ultrasound2k@gmail.com>
2017-01-25 03:45:15 +0900

    update post page

--- webapps/blogs/ultrasound/api/form.py
+++ webapps/blogs/ultrasound/api/form.py
@@ -2,7 +2,7 @@ from django.forms import ModelForm
 from .models import Post
 
 
-class FeedbackFrom(ModelForm):
+class postform(ModelForm):
     class Meta:
         model = Post
-        fields = ['username', 'title', 'content']
+        fields = ['id', 'username', 'title', 'content']
