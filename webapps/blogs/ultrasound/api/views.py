ccfa79f6abcb82a4adc2616dc8bb13ae83e2a601
Nick <ultrasound2k@gmail.com>
2017-01-25 01:04:42 +0900

    added Installed app in setting.py added project urls.py added app views.py and url.py

--- webapps/blogs/ultrasound/api/views.py
+++ webapps/blogs/ultrasound/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+def post_list(request):
+    data = [
+        {'id':1, 'title': 'title 1'},
+    ]
+    return JsonResponse(data, safe=False)
