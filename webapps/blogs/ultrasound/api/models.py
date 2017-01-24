749abc901cb342b11949478878f08232d919d0b9
Nick <ultrasound2k@gmail.com>
2017-01-25 03:18:10 +0900

    added login page

--- webapps/blogs/ultrasound/api/models.py
+++ webapps/blogs/ultrasound/api/models.py
@@ -3,7 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
+    user_id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=10)
+    is_author = models.CharField(max_lenth=10)
+    article_title = models.CharField(max_length=100)
+    article_content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- webapps/blogs/ultrasound/api/views.py
+++ webapps/blogs/ultrasound/api/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Post
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.contrib.auth.forms import UserCreationForm
 
-def post_list(request):
-    post_list = []
-    for post in Post.objects.all():
-        post_list.append({
-            'id':post.id,
-            'title':post.title,
-            'content':post.content,
-        })
-    return JsonResponse(post_list, safe=False)
+def signup(request):
+    """signup
+    to register users
+    """
+    if request.method == "POST":
+        userform = UserCreationForm(request.POST)
+        if userform.is_valid():
+            userform.save()
+
+        return HttpResponseRedirect(
+            reverse("signup_ok")
+        )
+    elif request.method == "GET":
+        userform = UserCreationForm()
+
+    return render(request, "registration/signup.html", {
+        "userform": userform,
+    })
